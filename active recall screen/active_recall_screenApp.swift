d5d2ad91284d6eafae0ffb3c53b2db28933cf570
Anvitha <anvithaatluri@gmail.com>
2021-11-15 11:20:45 +0800

    Initial Commit

---
+++ active recall screen/active_recall_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  active_recall_screenApp.swift
+//  active recall screen
+//
+//  Created by sap on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct active_recall_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
